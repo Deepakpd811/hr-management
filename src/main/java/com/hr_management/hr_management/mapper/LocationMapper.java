c50cf1389520a3ad74e7218e7adad9df39a7a064
AMIT KUMAR <amit70kumar14@gmail.com>
2025-06-07 09:52:41 +0530

    [Amit Kumar] Update : minor changes in location Mapper

---
+++ src/main/java/com/hr_management/hr_management/mapper/LocationMapper.java
@@ -0,0 +1,30 @@
+package com.hr_management.hr_management.mapper;
+
+import com.hr_management.hr_management.model.dto.LocationDTO;
+import com.hr_management.hr_management.model.entity.Location;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LocationMapper {
+    public LocationDTO toLocationDto(Location location) {
+        LocationDTO dto = new LocationDTO();
+        dto.setLocationId(location.getLocationId());
+        dto.setCity(location.getCity());
+        dto.setStreetAddress(location.getStreetAddress());
+        dto.setPostalCode(location.getPostalCode());
+        dto.setCountryName(location.getCountry());
+        dto.setCity(location.getCity());
+        return dto;
+    }
+
+    public Location toLocation(LocationDTO dto) {
+        Location location = new Location();
+        location.setLocationId(dto.getLocationId());
+        location.setCity(dto.getCity());
+        location.setStreetAddress(dto.getStreetAddress());
+        location.setPostalCode(dto.getPostalCode());
+        location.setStateProvince(dto.getStateProvince());
+        location.setCountry(dto.getCountryName());
+        return location;
+    }
+}
