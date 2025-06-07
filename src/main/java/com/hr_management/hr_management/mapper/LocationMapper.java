4f74cdacd155523b137c2e585a492e66d06f6206
AMIT KUMAR <amit70kumar14@gmail.com>
2025-06-07 09:59:20 +0530

    [Amit Kumar] Update : minor changes in location Mapper

--- src/main/java/com/hr_management/hr_management/mapper/LocationMapper.java
+++ src/main/java/com/hr_management/hr_management/mapper/LocationMapper.java
@@ -12,8 +12,19 @@ public class LocationMapper {
         dto.setCity(location.getCity());
         dto.setStreetAddress(location.getStreetAddress());
         dto.setPostalCode(location.getPostalCode());
-        dto.setCountryName(location.getCountry().getCountryName());
+        dto.setCountry(location.getCountry());
         dto.setCity(location.getCity());
         return dto;
+    }
+
+    public Location toLocation(LocationDTO dto) {
+        Location location = new Location();
+        location.setLocationId(dto.getLocationId());
+        location.setCity(dto.getCity());
+        location.setStreetAddress(dto.getStreetAddress());
+        location.setPostalCode(dto.getPostalCode());
+        location.setStateProvince(dto.getStateProvince());
+        location.setCountry(dto.getCountry());
+        return location;
     }
 }
