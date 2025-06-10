d450e0f47eaa65019044b0ef64472b511eb3d1cc
Divyam0463 <102290944+Divyam0463@users.noreply.github.com>
2025-06-10 21:59:39 +0530

    [Divyam] Some Changes

---
+++ src/main/java/com/hr_management/hr_management/exception/DuplicateRegionIdException.java
@@ -0,0 +1,8 @@
+package com.hr_management.hr_management.exception;
+
+public class DuplicateRegionIdException extends RuntimeException {
+    public DuplicateRegionIdException(String message) {
+        super(message);
+    }
+}
+
